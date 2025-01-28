5a884c052380bc004469a0a6a68c6d06518d5b50
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-01-28 23:24:34 +0600

    Update Category

---
+++ src/components/CategoryList.tsx
@@ -0,0 +1,111 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const CategoryList = () => {
+  return (
+    <div className="px-4 overflow-x-scroll scrollbar-hide">
+      <div className="flex gap-4 md:gap-8">
+        <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+          <div className="relative w-full h-96 bg-slate-100">
+            <Image
+              src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+              fill
+              sizes="25vw"
+              className="object-cover"
+            />
+          </div>
+          <h2 className="text-xl mt-8 font-light tracking-wide">
+            Category name
+          </h2>
+        </Link>
+        <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+          <div className="relative w-full h-96 bg-slate-100">
+            <Image
+              src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+              fill
+              sizes="25vw"
+              className="object-cover"
+            />
+          </div>
+          <h2 className="text-xl mt-8 font-light tracking-wide">
+            Category name
+          </h2>
+        </Link>
+        <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+          <div className="relative w-full h-96 bg-slate-100">
+            <Image
+              src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+              fill
+              sizes="25vw"
+              className="object-cover"
+            />
+          </div>
+          <h2 className="text-xl mt-8 font-light tracking-wide">
+            Category name
+          </h2>
+        </Link>
+        <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+          <div className="relative w-full h-96 bg-slate-100">
+            <Image
+              src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+              fill
+              sizes="25vw"
+              className="object-cover"
+            />
+          </div>
+          <h2 className="text-xl mt-8 font-light tracking-wide">
+            Category name
+          </h2>
+        </Link>
+        <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+          <div className="relative w-full h-96 bg-slate-100">
+            <Image
+              src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+              fill
+              sizes="25vw"
+              className="object-cover"
+            />
+          </div>
+          <h2 className="text-xl mt-8 font-light tracking-wide">
+            Category name
+          </h2>
+        </Link>
+        <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+          <div className="relative w-full h-96 bg-slate-100">
+            <Image
+              src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+              fill
+              sizes="25vw"
+              className="object-cover"
+            />
+          </div>
+          <h2 className="text-xl mt-8 font-light tracking-wide">
+            Category name
+          </h2>
+        </Link>
+        <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+          <div className="relative w-full h-96 bg-slate-100">
+            <Image
+              src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+              fill
+              sizes="25vw"
+              className="object-cover"
+            />
+          </div>
+          <h2 className="text-xl mt-8 font-light tracking-wide">
+            Category name
+          </h2>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,107 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex justify-between gap-x-8 gap-y-16 flex-wrap">
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="relative w-full h-80 ">
+          <Image
+            src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-pinki text-pinki w-max py-2 px-4 text-xs hover:bg-pinki hover:text-white">Add to cart</button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="relative w-full h-80 ">
+          <Image
+            src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-pinki text-pinki w-max py-2 px-4 text-xs hover:bg-pinki hover:text-white">Add to cart</button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="relative w-full h-80 ">
+          <Image
+            src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-pinki text-pinki w-max py-2 px-4 text-xs hover:bg-pinki hover:text-white">Add to cart</button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="relative w-full h-80 ">
+          <Image
+            src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-pinki text-pinki w-max py-2 px-4 text-xs hover:bg-pinki hover:text-white">Add to cart</button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
